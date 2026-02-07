771358b2b8001d65f1774fa550fea7e998aad381
jyothiprakash425-DE <jyothiprakash425@gmail.com>
2026-02-07 14:18:12 +0530

    file operations script

---
+++ test.py
@@ -0,0 +1,4 @@
+Hi This is jyothi prakash
+I have 11+ years of experience as Data Engineer.
+I have strong skills in RDBMS databases,ETL-SSIS & ADF ,Spark/Pyspark,databricks and python.
+I had been worked seven years in worldline and now working as manager in genpact.
